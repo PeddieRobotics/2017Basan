4b6ab27a31f7c2a02fc6404d46adb14c6ef8221b
Maya Sim <hsim-19@peddie.org>
2017-01-23 17:00:00 -0500

    another update on intake

--- src/org/usfirst/frc/team5895/robot/Intake.java
+++ src/org/usfirst/frc/team5895/robot/Intake.java
@@ -28,28 +28,33 @@ public Intake() {
     isDown = false;
 }
 
+   //Make an intake subsystem that has a motor and a solenoid.
+   //When the solenoid is on the intake will be down and the motor should be spinning.
+   //When the intake is up the motor should be off.
+   
+   //solenoid on => intake down = motor on
+   //              => intake up = motor off
 public void up() {
-    isDown = false;
-    speed = 0.0;
+   isDown = false;
+   speed = 0.0;
 }
 
 public void down() {
-    isDown = true;
-    speed = 0.5;
+   isDown = true;
+   speed = 0.5;
 }
 
 public void update() {
-    motor.set(speed);
-    
-    if (myCylinder.get() != isDown)
-        myCylinder.set(isDown);
-    
+    controlCurrent();
+   motor.set(speed);
+   if (myCylinder.get() != isDown)
+       myCylinder.set(isDown);
+   
 }
 public void controlCurrent(){
     double limit = 5.0;
     if (pDP.getCurrent(3)>=limit){
-        motor.setSpeed(0.0);
+        speed=0;
     }
 }
-
 }
