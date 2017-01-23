8643bc633770f80ef7cecec86c29afc2f7671ddf
MahBoiVladdyP <trev0407@gmail.com>
2017-01-23 17:17:31 -0500

    Updated DriveTrain Code

--- src/org/usfirst/frc/team5895/robot/DriveTrain.java
+++ src/org/usfirst/frc/team5895/robot/DriveTrain.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team5895.robot;
 
+import org.usfirst.frc.team5895.robot.lib.NavX;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
@@ -11,14 +13,18 @@ public class DriveTrain {
 	private enum Mode_Type {TELEOP, AUTO};
 	private Mode_Type mode = Mode_Type.AUTO;
 	private Encoder Eleft, Eright;
-	private double dist;
-	private final double Kp=0.00002;
-	private double setpoint;
-	private double error;
-	private PID PID;
+	private NavX NavX;
+	private final double Kpstraight=0.02;
+	private final double Kpturn = 0.07;
+	private final double Ki=0.0000005;
+	private final double Kd=0.0001;
+	private PID PIDStraight;
+	private PID PIDTurning;
 	
 	public DriveTrain()
 	{
+		NavX=new NavX();
+		
 		Mleft = new Talon(1);
 		Mright = new Talon(0);
 		
@@ -28,7 +34,9 @@ public class DriveTrain {
 		Eleft.setDistancePerPulse(4*3.14/360);
 		Eright.setDistancePerPulse(4*3.14/360);
 		
-		PID = new PID(1, 0, 0, 1);
+		PIDStraight = new PID(Kpstraight, Ki, Kd, 1);
+		PIDTurning= new PID(Kpturn, 0, 0, 1);
+		
 		
 	}
 	
@@ -41,11 +49,21 @@ public class DriveTrain {
 		return distance;
 	}
 	
-	public void autoDrive() {
-
-		dist = getDistance();
+	public double getAngle(){
+		double angle = NavX.getAngle();
 		
-		PID.set(72);
+		return angle;
+	}
+	
+	public void resetEncodersAndNavX(){
+		Eleft.reset();
+		Eright.reset();
+		NavX.reset();
+	}
+	
+	public void autoDrive() {		
+		PIDStraight.set(72);
+		PIDTurning.set(0);
 		
 		mode = Mode_Type.AUTO;
 	}
@@ -58,14 +76,21 @@ public class DriveTrain {
 	
 	public void update()
 	{
-		DriverStation.reportError("distance = " + getDistance(), false);
+		DriverStation.reportError("distance = " + getDistance()+"\n", false);
+		//DriverStation.reportError("angle = " + getAngle()+"\n", false);
 		
 		switch(mode) {
 		case AUTO:
-			double output = PID.getOutput(dist);
-			Mleft.set(-output);
-			Mright.set(output);
-			
+			double output = PIDStraight.getOutput(getDistance());
+			double spinningOutput = -PIDTurning.getOutput(getAngle());
+			/*if(output>=0.5){
+				output=0.5;
+			}
+			else if(output<=-0.5){
+				output=-0.5;
+			}*/
+			Mleft.set(-output+spinningOutput);
+			Mright.set(output+spinningOutput);
 			break;
 			
 		case TELEOP:
