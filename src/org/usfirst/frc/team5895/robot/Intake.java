036b0b6a83f5415a3fe5d656ff16d15d83341d6f
Antonio G Papa <antoniogpapa@gmail.com>
2017-02-04 18:13:59 -0500

    Indented Intake

--- src/org/usfirst/frc/team5895/robot/Intake.java
+++ src/org/usfirst/frc/team5895/robot/Intake.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team5895.robot;
 
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
 
@@ -17,35 +16,26 @@ public class Intake {
     private Solenoid myCylinder;
     private boolean isDown;
 
-}
-
-   //Make an intake subsystem that has a motor and a solenoid.
-   //When the solenoid is on the intake will be down and the motor should be spinning.
-   //When the intake is up the motor should be off.
-   
-   //solenoid on => intake down = motor on
-   //              => intake up = motor off
-public void up() {
-   isDown = false;
-   speed = 0.0;
-}
-
-public void down() {
-   isDown = true;
-   speed = 0.5;
-}
-
-public void update() {
-    controlCurrent();
-   motor.set(speed);
-   if (myCylinder.get() != isDown)
-       myCylinder.set(isDown);
-   
-}
-public void controlCurrent(){
-    double limit = 5.0;
-    if (pDP.getCurrent(3)>=limit){
-        speed=0;
+    public Intake() {
+   	    motor = new Talon(2);
+   	    speed = 0;
+   	    myCylinder = new Solenoid(0);
+   	    isDown = false;
     }
-}
+    
+	public void up() {
+	   isDown = false;
+	   speed = 0.0;
+	}
+	
+	public void down() {
+	   isDown = true;
+	   speed = 0.5;
+	}
+	
+	public void update() {
+	   motor.set(speed);
+	   if (myCylinder.get() != isDown)
+	       myCylinder.set(isDown);
+	}
 }
