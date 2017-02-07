1721d6c183a2ed6f4cfa0bac33d821fdf1e9fc25
Antonio G Papa <antoniogpapa@gmail.com>
2017-02-07 16:49:27 -0500

    Fixed Drivetrain to use Trajectory (again)

--- src/org/usfirst/frc/team5895/robot/DriveTrain.java
+++ src/org/usfirst/frc/team5895/robot/DriveTrain.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team5895.robot;
 
 import org.usfirst.frc.team5895.robot.lib.NavX;
-import org.usfirst.frc.team5895.robot.lib.PID;
+import org.usfirst.frc.team5895.robot.lib.TrajectoryDriveController;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
@@ -12,99 +12,95 @@ public class DriveTrain {
 	private Talon Mright;
 	double Lspeed, Rspeed;
 	private enum Mode_Type {TELEOP, AUTO};
-	private Mode_Type mode = Mode_Type.AUTO;
+	private Mode_Type mode = Mode_Type.TELEOP;
 	private Encoder Eleft, Eright;
 	private NavX NavX;
-	private final double Kpstraight=0.02;
-	private final double Kpturn = 0.07;
-	private final double Ki=0.0000005;
-	private final double Kd=0.0001;
-	private PID PIDStraight;
-	private PID PIDTurning;
-
+	private TrajectoryDriveController c;
+	
 	public DriveTrain()
 	{
 		NavX=new NavX();
-
-		Mleft = new Talon(DRIVE_LEFTMOTOR);
-		Mright = new Talon(DRIVE_RIGHTMOTOR);
-
-		Eleft = new Encoder(LEFTDRIVE_ENCODER,LEFTDRIVE_ENCODER2);
-		Eright = new Encoder(RIGHTDRIVE_ENCODER,RIGHTDRIVE_ENCODER2);
-
-		Eleft.setDistancePerPulse(-1* 4*3.14/360);
-		Eright.setDistancePerPulse(4*3.14/360);
-
-		PIDStraight = new PID(Kpstraight, Ki, Kd, 1);
-		PIDTurning= new PID(Kpturn, 0, 0, 1);
-
+		
+		Mleft = new Talon(ElectricalLayout.DRIVE_LEFTMOTOR);
+		Mright = new Talon(ElectricalLayout.DRIVE_RIGHTMOTOR);
+		
+		Eleft = new Encoder(2,3, false, Encoder.EncodingType.k4X);
+		Eright = new Encoder(0,1, false, Encoder.EncodingType.k4X);
+		
+		Eleft.setDistancePerPulse(4/12.0*3.14/360);
+		Eright.setDistancePerPulse(4/12.0*3.14/360);
+		
+		c = new TrajectoryDriveController("/home/lvuser/Turn.txt", 0, 0, 0, 1.0/14.6, 1.0/45.0, -0.02);
+		
 	}
-
+	
 
 	public double getDistance() {
-
-		double distance = ((Eleft.getDistance()) + Eright.getDistance())/2;
-
+	
+		double distance = ((-1*Eleft.getDistance()) + Eright.getDistance())/2;
+		
 		return distance;
 	}
-
+	
+	public double getSpeed() {
+		return ((-1*Eleft.getRate()) + Eright.getRate())/2;
+	}
+	
 	public double getAngle(){
 		double angle = NavX.getAngle();
-
+		
 		return angle;
 	}
-
+	
 	public void resetEncodersAndNavX(){
 		Eleft.reset();
 		Eright.reset();
 		NavX.reset();
 	}
-
+	
 	public void autoDrive() {
-		PIDStraight.set(72);
-		PIDTurning.set(0);
-
+		resetEncodersAndNavX();
+		c.reset();
 		mode = Mode_Type.AUTO;
 	}
-
+	
 	public void arcadeDrive( double speed, double turn) {
 		Lspeed = speed + turn;
 		Rspeed = -speed + turn;
 		mode = Mode_Type.TELEOP;
 	}
-
+	
 	public void setLeftRightPower(double l, double r) {
 		Lspeed = l;
 		Rspeed = r;
 		mode = Mode_Type.TELEOP;
 	}
-
+	
 	public void update()
 	{
-		DriverStation.reportError("distance = " + getDistance()+"\n", false);
-		//DriverStation.reportError("angle = " + getAngle()+"\n", false);
-
+		//DriverStation.reportError("distance = " + getDistance()+"\n", false);
+		
 		switch(mode) {
 		case AUTO:
-			double output = PIDStraight.getOutput(getDistance());
-			double spinningOutput = -PIDTurning.getOutput(getAngle());
-			/*if(output>=0.5){
-				output=0.5;
-			}
-			else if(output<=-0.5){
-				output=-0.5;
-			}*/
-			Mleft.set(-output+spinningOutput);
-			Mright.set(output+spinningOutput);
+			
+			double[] m = new double[2];
+			
+			m = c.getOutput(Eleft.getDistance(), Eright.getDistance(), getAngle()*3.14/180);
+			
+			DriverStation.reportError("the distance is" + getDistance(), false);
+			
+			Mleft.set(m[0]);
+			Mright.set(-m[1]);
 			break;
-
+			
 		case TELEOP:
+			
 			Mleft.set(Lspeed);
 			Mright.set(Rspeed);
-
+			
 			break;
-
+		
 	}
-
+	
 }
 }
