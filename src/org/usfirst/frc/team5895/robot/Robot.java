7c98448e19396b0a4c7044a6a31c5d106259dd4f
Cameron Hochberg <caos2804@gmail.com>
2017-02-08 14:46:16 -0500

    Added vision to the loop

--- src/org/usfirst/frc/team5895/robot/Robot.java
+++ src/org/usfirst/frc/team5895/robot/Robot.java
@@ -9,29 +9,35 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 public class Robot extends IterativeRobot {
 
-	Looper loop;
+	Looper loop, loop_vision;
 	Joystick Jleft, Jright;
 	DriveTrain drivetrain;
-	
+  Vision vision;
+
     public void robotInit() {
-    	
+
     	Jleft = new Joystick(0);
     	Jright = new Joystick(1);
     	drivetrain = new DriveTrain();
-    	
+      vision = new Vision();
+
     	loop = new Looper(10);
     	loop.add(drivetrain::update);
     	loop.start();
+
+      loop_vision = new Looper(250);
+      loop_vision.add(vision::update);
+      loop_vision.start();
     }
-    
+
     public void autonomousInit() {
     }
-    
+
     public void teleopPeriodic() {
     	drivetrain.arcadeDrive(Jleft.getRawAxis(1), Jright.getRawAxis(0));
     }
-    
+
     public void disabledInit() {
-    
+
     }
 }
