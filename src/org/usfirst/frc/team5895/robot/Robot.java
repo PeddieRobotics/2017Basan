d8e5ba47e8e09bdc954156d9222d81a0e8878305
Antonio G Papa <antoniogpapa@gmail.com>
2017-02-13 14:55:02 -0500

    Gave autonomous example to delete later

--- src/org/usfirst/frc/team5895/robot/Robot.java
+++ src/org/usfirst/frc/team5895/robot/Robot.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5895.robot;
 
+import org.usfirst.frc.team5895.robot.auto.*;
 import org.usfirst.frc.team5895.robot.framework.*;
 import org.usfirst.frc.team5895.robot.lib.TrajectoryDriveController;
 import org.usfirst.frc.team5895.robot.lib.trajectory.TextFileReader;
@@ -7,11 +8,14 @@ import org.usfirst.frc.team5895.robot.lib.trajectory.TextFileReader;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends IterativeRobot {
 
-	Looper loop, loop_vision;
+	Looper loop;
 	Joystick Jleft, Jright;
 	DriveTrain drivetrain;
+	Shooter shooter;
+	Turret turret;
 	Vision vision;
 
     public void robotInit() {
@@ -19,18 +23,24 @@ public class Robot extends IterativeRobot {
     	Jleft = new Joystick(0);
     	Jright = new Joystick(1);
     	drivetrain = new DriveTrain();
+    	shooter = new Shooter();
+    	turret = new Turret();
     	vision = new Vision();
 
     	loop = new Looper(10);
     	loop.add(drivetrain::update);
     	loop.start();
 
-	    loop_vision = new Looper(250);
-	    loop_vision.add(vision::update);
-	    loop_vision.start();
     }
 
     public void autonomousInit() {
+    	String routine = SmartDashboard.getString("DB/String 0", "nothing");
+    	
+    	if (routine.contains("spin")) {
+    		SpinInCirclesShootingWildly.run(drivetrain, shooter, turret);
+    	} else {
+    		DoNothing.run();
+    	}
     }
 
     public void teleopPeriodic() {
@@ -38,6 +48,7 @@ public class Robot extends IterativeRobot {
     }
 
     public void disabledInit() {
-
+    	drivetrain.arcadeDrive(0, 0);
+    	shooter.setSpeed(0);
     }
 }
