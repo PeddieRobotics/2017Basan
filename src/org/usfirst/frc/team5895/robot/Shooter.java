f47c41fbe2764035145927fd9359b0eea92fae4b
CatherinePhillips <cphillips-19@peddie.org>
2017-02-17 16:17:45 -0500

    Flywheel PID

--- src/org/usfirst/frc/team5895/robot/Shooter.java
+++ src/org/usfirst/frc/team5895/robot/Shooter.java
@@ -9,20 +9,26 @@ public class Shooter {
 
 	private Talon flywheelMotor;
 	private Talon conveyorMotor;
+	private Solenoid hood;
 	private double speed;
+	boolean hoodPosition;
 
 	PID PID;
 	Counter Counter;
 
-	double Kp = 0.05;
+	double Kp = 0.2;
+	double Ki = 0.00001;
+	double Kd = 0.00000005;
+	double dV = 1;
 
 
 	public Shooter()
 	{
 		flywheelMotor = new Talon(ElectricalLayout.FLYWHEEL_MOTOR);
 		conveyorMotor = new Talon(ElectricalLayout.CONVEYOR_MOTOR);
+		hood = new Solenoid(ElectricalLayout.FLYWHEEL_SOLENOID);
 
-		PID = new PID(Kp, 0, 0, 0);
+		PID = new PID(Kp, Ki, Kd, dV);
 		Counter = new Counter(ElectricalLayout.FLYWHEEL_COUNTER);
 		Counter.setDistancePerPulse(1);
 		
@@ -51,13 +57,26 @@ public class Shooter {
 		speed = -0.6;
 	}
 	
+	/**
+	 * hood is on the big angle
+	 */
+	public void hoodUp(){
+		hoodPosition = true; 
+	}
+	
+	/**
+	 * hood is on the small angle
+	 */
+	public void hoodDown(){
+		hoodPosition = false;
+	}
 	
 	/**
 	 * return the speed of the fly wheel in RPM
 	 * @return the speed, in RPM, of the fly wheel
 	 */
 	public double getSpeed() {
-		double flywheelSpeed = Counter.getRate()/60;
+		double flywheelSpeed = Counter.getRate()*60;
 		return flywheelSpeed;
 	}
 	
@@ -84,6 +103,7 @@ public class Shooter {
 	public void update() {
 		flywheelMotor.set(PID.getOutput(Counter.getRate()));
 		conveyorMotor.set(speed);
+		hood.set(hoodPosition);
 	}
 }
 
