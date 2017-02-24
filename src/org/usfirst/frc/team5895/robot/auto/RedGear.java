a072c52ea2e20bde452c61ed79d60185e72004ba
CatherinePhillips <cphillips-19@peddie.org>
2017-02-24 16:01:48 -0500

    Gear Auto Modes

---
+++ src/org/usfirst/frc/team5895/robot/auto/RedGear.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team5895.robot.auto;
+
+import org.usfirst.frc.team5895.robot.DriveTrain;
+import org.usfirst.frc.team5895.robot.GearReceiver;
+import org.usfirst.frc.team5895.robot.framework.Waiter;
+
+public class RedGear {
+	
+	public static void run(DriveTrain drivetrain, GearReceiver gear) {
+		
+		//Gear side is front
+		drivetrain.driveTo(-125);
+		Waiter.waitFor(drivetrain::atDistance, 5000.0);
+		drivetrain.turnTo(-60);
+		Waiter.waitFor(drivetrain::atAngle, 5000);
+		gear.open();
+		drivetrain.driveTo(-10);
+		
+	}
+
+}
