1a1e746e76e6ea7d221da2c6d8c120052ee72962
Cameron Hochberg <caos2804@gmail.com>
2017-03-04 13:19:31 -0500

    Added check on shooter in autonomous
    If the flywheel doesn't run then don't run the shooter in autonomous

--- src/org/usfirst/frc/team5895/robot/auto/RedAuto.java
+++ src/org/usfirst/frc/team5895/robot/auto/RedAuto.java
@@ -8,17 +8,18 @@ import org.usfirst.frc.team5895.robot.framework.LookupTable;
 import org.usfirst.frc.team5895.robot.framework.Waiter;
 
 public class RedAuto {
-	
+
 	public static void run(DriveTrain drivetrain, Shooter shooter, Turret turret, LookupTable table, Vision vision) {
-		
+
 		turret.turnTo(70);
 		drivetrain.auto_balls_redDrive();
 		Waiter.waitFor(drivetrain::trajectoryFinished,5000);
 		turret.turnTo(turret.getAngle()+vision.getX());
 		shooter.setSpeed(table.get(vision.getDist()));
 		Waiter.waitFor(shooter::atSpeed, 2000);
-		shooter.shoot();
-		
+		if(shooter.getSpeed()>0){
+			shooter.shoot();
+		}
 	}
 
 }
