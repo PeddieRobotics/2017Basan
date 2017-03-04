f62fe9bf0fe3cc7c0a8378bbc064678bfae78523
Cameron Hochberg <caos2804@gmail.com>
2017-03-04 13:23:49 -0500

    Changed Angle of Turret in BlueAuto to match boiler

--- src/org/usfirst/frc/team5895/robot/auto/BlueAuto.java
+++ src/org/usfirst/frc/team5895/robot/auto/BlueAuto.java
@@ -11,13 +11,15 @@ public class BlueAuto {
 
 	public static void run(DriveTrain drivetrain, Shooter shooter, Turret turret, LookupTable table, Vision vision) {
 		
-		turret.turnTo(70);
+		turret.turnTo(-70);
 		drivetrain.auto_balls_blueDrive();
 		Waiter.waitFor(drivetrain::trajectoryFinished,5000);
 		turret.turnTo(turret.getAngle()+vision.getX());
 		shooter.setSpeed(table.get(vision.getDist()));
 		Waiter.waitFor(shooter::atSpeed, 2000);
-		shooter.shoot();
+		if(shooter.getSpeed()>0){
+			shooter.shoot();
+		}
 		
 	}
 	
