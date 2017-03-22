1f271965c95a34858d9ef4fd5a9761ae59718116
Cameron Hochberg <caos2804@gmail.com>
2017-03-21 20:24:40 -0400

    Auto codes from testing today

--- src/org/usfirst/frc/team5895/robot/auto/BlueAuto.java
+++ src/org/usfirst/frc/team5895/robot/auto/BlueAuto.java
@@ -9,19 +9,24 @@ import org.usfirst.frc.team5895.robot.framework.LookupTable;
 import org.usfirst.frc.team5895.robot.framework.Waiter;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class BlueAuto {
 
 	public static void run(DriveTrain drivetrain, Shooter shooter, Turret turret, LookupTable table, Vision vision, Intake intake) {
 		
 		turret.turnTo(-80);
-		intake.down();
-		drivetrain.auto_blueDrive();
-		Waiter.waitFor(5000);
-		drivetrain.arcadeDrive(0, 0);
+//		intake.down();
+//		drivetrain.auto_blueDrive();
+		Waiter.waitFor(1000);
+//		drivetrain.arcadeDrive(0, 0);
 		vision.update();
 		turret.turnTo(turret.getAngle()+vision.getX());
-		shooter.setSpeed(table.get(vision.getDist()));
+		Waiter.waitFor(200);
+		vision.update();
+		turret.turnTo(turret.getAngle()+vision.getX());
+		shooter.setSpeed(SmartDashboard.getNumber("DB/Slider 0", 0));
+//		shooter.setSpeed(table.get(vision.getDist()));
 		Waiter.waitFor(shooter::atSpeed, 2000);
 		if(shooter.getSpeed() > 10) {
 			shooter.shoot();
--- src/org/usfirst/frc/team5895/robot/auto/RedAuto.java
+++ src/org/usfirst/frc/team5895/robot/auto/RedAuto.java
@@ -8,18 +8,24 @@ import org.usfirst.frc.team5895.robot.Vision;
 import org.usfirst.frc.team5895.robot.framework.LookupTable;
 import org.usfirst.frc.team5895.robot.framework.Waiter;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class RedAuto {
 	
 	public static void run(DriveTrain drivetrain, Shooter shooter, Turret turret, LookupTable table, Vision vision, Intake intake) {
 	
 		turret.turnTo(80);
-		intake.down();
-		drivetrain.auto_redDrive();
-		Waiter.waitFor(5000);
-		drivetrain.arcadeDrive(0, 0);
+		//intake.down();
+		//drivetrain.auto_redDrive();
+		Waiter.waitFor(1000);
+		//drivetrain.arcadeDrive(0, 0);
 		vision.update();
 		turret.turnTo(turret.getAngle()+vision.getX());
-		shooter.setSpeed(table.get(vision.getDist()));
+		Waiter.waitFor(200);
+		vision.update();
+		turret.turnTo(turret.getAngle()+vision.getX());
+		shooter.setSpeed(SmartDashboard.getNumber("DB/Slider 0", 0));
+		//shooter.setSpeed(table.get(vision.getDist()));
 		Waiter.waitFor(shooter::atSpeed, 2000);
 		if(shooter.getSpeed() > 10) {
 			shooter.shoot();
